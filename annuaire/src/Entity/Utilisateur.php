212444bedd76c733165a1ec519caf3998e61d84c
Enzo Guilhot <enzo.guilhot@etu.umontpellier.fr>
2024-09-17 11:44:21 +0200

    feat : connexion à la bd

---
+++ annuaire/src/Entity/Utilisateur.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $login = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private ?bool $visible = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $codeUnique = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateEdition = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateConnexion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): static
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function isVisible(): ?bool
+    {
+        return $this->visible;
+    }
+
+    public function setVisible(bool $visible): static
+    {
+        $this->visible = $visible;
+
+        return $this;
+    }
+
+    public function getCodeUnique(): ?string
+    {
+        return $this->codeUnique;
+    }
+
+    public function setCodeUnique(string $codeUnique): static
+    {
+        $this->codeUnique = $codeUnique;
+
+        return $this;
+    }
+
+    public function getDateEdition(): ?\DateTimeInterface
+    {
+        return $this->dateEdition;
+    }
+
+    public function setDateEdition(\DateTimeInterface $dateEdition): static
+    {
+        $this->dateEdition = $dateEdition;
+
+        return $this;
+    }
+
+    public function getDateConnexion(): ?\DateTimeInterface
+    {
+        return $this->dateConnexion;
+    }
+
+    public function setDateConnexion(\DateTimeInterface $dateConnexion): static
+    {
+        $this->dateConnexion = $dateConnexion;
+
+        return $this;
+    }
+}
