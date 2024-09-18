20b39bb35e49cc33a1dc682a67e70598c869b702
Enzo Guilhot <enzo.guilhot@etu.umontpellier.fr>
2024-09-18 15:06:10 +0200

    feat : detailProfil

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProfilRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,5 +15,12 @@ class UtilisateurController extends AbstractController
         return $this->render('utilisateur/liste.html.twig', [
 
         ]);
+    }
+
+    #[Route('/profil/{code}', 'detailProfil', methods: ['GET'])]
+    public function hello2($code, ProfilRepository $profilRepository): Response
+    {
+        $profil = $profilRepository->findOneBy(['codeUnique' => $code]);
+        return $this->render("", ["profil" => $profil]);
     }
 }
