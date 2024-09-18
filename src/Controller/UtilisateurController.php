3947fc4c64704667c5162a3b600b7272806ecfd2
Raphaël/ <izoret@proton.me>
2024-09-18 14:50:24 +0200

    [Feature] Prototype liste utilisateurs avec BD

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UtilisateurController extends AbstractController
 {
+    public function __construct(
+        private readonly UtilisateurRepository $utilisateurRepo
+    )
+    {
+    }
+
     #[Route('/', name: 'liste')]
     public function liste(): Response
     {
+        $utilisateurs = $this->utilisateurRepo->findAll();
         return $this->render('utilisateur/liste.html.twig', [
-
+            'utilisateurs' => $utilisateurs,
         ]);
     }
 }
