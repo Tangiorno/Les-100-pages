e3b7cee9ae6fae4bf7d78d23fb17468d42d55dba
Raphaël/ <izoret@proton.me>
2024-09-20 13:03:33 +0200

    [Feature] popup de confirmation + importation js

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,24 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+console.log("JavaScript is running!")
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+function handleDeletePopup() {
+    document.getElementById('confirmation-modal').style.display = 'block';
+
+    document.getElementById('close-modal').addEventListener('click', function () {
+        document.getElementById('confirmation-modal').style.display = 'none';
+    });
+
+    document.getElementById('cancel-delete').addEventListener('click', function () {
+        document.getElementById('confirmation-modal').style.display = 'none';
+    });
+
+    document.getElementById('confirm-delete').addEventListener('click', function () {
+        document.getElementById('delete-form').submit();
+    });
+
+    window.onclick = function (event) {
+        const modal = document.getElementById('confirmation-modal');
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    };
+}
