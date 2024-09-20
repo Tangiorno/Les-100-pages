9a8fe7a47cb22dc6930f7158c7d21f01ea05cbcf
Raphaël/ <izoret@proton.me>
2024-09-20 11:19:26 +0200

    [Feature] enlevé infos non-essentielles du formulaire d'inscription

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -29,11 +29,11 @@ class UtilisateurType extends AbstractType
                 [new NotBlank(), new NotNull(),
                     new Length(["min" => 8, "max" => 50, "minMessage" => "Le mot de passe doit posséder au minimum 8 caractères", "maxMessage" => "Le mot de passe doit posséder au maximum 50 caractères"]),
                     new Regex("#^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,30}$#", "Le mot de passe doit contenir au minimum une majuscule, une minuscule et un chiffre")]])
-            ->add('visible', CheckboxType::class, ['required' => false, 'data' => true])
             ->add('codeUnique', TextType::class, ["required" => false])
-            ->add('numeroTelephone', TextType::class)
-            ->add('pays', TextType::class)
-            /*->add('dateEdition', null, [
+            ->add('visible', CheckboxType::class, ['required' => false, 'data' => true]) //data = true check la box par défaut
+            /*            ->add('numeroTelephone', TextType::class)
+                        ->add('pays', TextType::class)
+              */          /*->add('dateEdition', null, [
                 'widget' => 'single_text',
             ])
             ->add('dateConnexion', null, [
