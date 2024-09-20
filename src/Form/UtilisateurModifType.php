14503aacdd7d7657b34c918f81ae7ae3d1b55482
FX506 <FX506@MacBook-Air.local>
2024-09-20 14:18:39 +0200

    feat: edition

--- src/Form/UtilisateurModifType.php
+++ src/Form/UtilisateurModifType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +17,16 @@ class UtilisateurModifType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('login')
-            ->add('prenom')
-            ->add('nom')
-            ->add('email')
-            ->add('codeUnique')
-            ->add('password')
-            ->add('visible')
-            ->add('numeroTelephone')
-            ->add('pays')
+            ->add('login', TextType::class, ["required" => false])
+            ->add('prenom', TextType::class, ["required" => false])
+            ->add('nom', TextType::class, ["required" => false])
+            ->add('email', EmailType::class, ["required" => false])
+            ->add('codeUnique', TextType::class, ["required" => false])
+            ->add('password', PasswordType::class, ["required" => false])
+            ->add('visible', CheckboxType::class)
+            ->add('numeroTelephone', TextType::class, ["required" => false])
+            ->add('pays', TextType::class, ["required" => false])
+            ->add('edition', SubmitType::class)
         ;
     }
 
