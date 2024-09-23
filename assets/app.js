c9917131c03844cb71f990a449759093b2c933fd
Raphaël/ <izoret@proton.me>
2024-09-23 14:36:28 +0200

    [Feature] tweak js + enlevé attribut profil dans la bd

--- assets/app.js
+++ assets/app.js
@@ -34,7 +34,7 @@ window.queryAJAX = () => {
         clearTimeout(timeout);
         timeout = setTimeout(function () {
             checkEmail(emailInput.value);
-        }, 500);
+        }, 100);
     });
     codeUniqueInput.addEventListener('input', function () {
         clearTimeout(timeout);
--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -42,9 +42,6 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     private ?DateTimeInterface $dateConnexion = null;
 
     #[ORM\Column]
-    private ?bool $profil = null;
-
-    #[ORM\Column]
     private ?bool $visible = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
@@ -192,17 +189,6 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     public function setRoles(?array $roles): void
     {
         $this->roles = $roles;
-    }
-
-
-    public function getProfil(): ?bool
-    {
-        return $this->profil;
-    }
-
-    public function setProfil(?bool $profil): void
-    {
-        $this->profil = $profil;
     }
 
     /**
