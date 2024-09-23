826843bee758f4555b396f59db4d7d3ff4621300
FX506 <FX506@MacBook-Air.local>
2024-09-23 14:27:32 +0200

    fix: maintenance

--- src/Controller/MaintenanceController.php
+++ src/Controller/MaintenanceController.php
@@ -3,16 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MaintenanceController extends AbstractController
 {
 
+    private ParameterBagInterface $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
+
     #[Route("/maintenance", name: 'maintenance', methods: ['GET'])]
 
     public function maintenance(): Response
     {
-        return $this->render('maintenance.html.twig');
+        if($this->params->get("maintenance_mode")){
+            return $this->render('maintenance.html.twig');
+        }
+        else{
+            $this->addFlash("error", "Accès non autorisé");
+            return $this->redirectToRoute("liste");
+        }
     }
 }
